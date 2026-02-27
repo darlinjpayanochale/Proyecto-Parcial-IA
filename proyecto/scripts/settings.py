42451093e527ae16e9f245b23fc51e7709e26851
Darlin Javier Payano Chale <darlinjpayanochale@gmail.com>
2026-02-27 14:35:17 -0400

    agregar configuracion del juego

---
+++ proyecto/scripts/settings.py
@@ -0,0 +1,18 @@
+# Nombre: Darlin Javier Payano Chale
+# Matrícula: 21-MISN-2-034
+
+# Configuración general del juego
+
+TILE_SIZE = 40  # Tamaño de cada celda del grid
+
+MAP_ROWS = 15
+MAP_COLS = 20
+
+FPS = 60
+
+# Colores
+COLOR_FLOOR = (30, 30, 30)
+COLOR_WALL = (100, 100, 100)
+COLOR_PLAYER = (0, 200, 0)
+COLOR_ENEMY = (200, 0, 0)
+COLOR_TREASURE = (255, 215, 0)
