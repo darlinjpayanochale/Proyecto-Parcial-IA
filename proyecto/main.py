f84276be293a061b861bba48071c02a58d9cf7ba
Darlin Javier Payano Chale <darlinjpayanochale@gmail.com>
2026-02-27 20:38:04 -0400

    Mejora de mensajes dentro del juego

--- proyecto/main.py
+++ proyecto/main.py
@@ -13,12 +13,15 @@ def main():
     # Inicializar pygame
     pygame.init()
 
+    font = pygame.font.SysFont(None, 36)
+
     # Configurar pantalla completa
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.display.set_caption("El Guardián del Tesoro")
     game_map = GameMap()
     player = Player(game_map)
     treasure = Treasure(game_map, player)
+    message = ""
 
     clock = pygame.time.Clock()
     running = True
@@ -32,12 +35,12 @@ def main():
                 
             if treasure.check_collision() and not player.has_treasure:
                     player.has_treasure = True
-                    print("Tesoro recogido ¡Regresa a la puerta!") 
+                    message = "Tesoro recogido ¡Regresa a la puerta!" 
 
 
             if player.has_treasure:
                  if player.row == player.start_row and player.col == player.start_col:
-                      print("¡Ganaste! Volviste a la puerta con el tesoro.")               
+                      message = "¡Ganaste!"              
 
             # Permitir salir con ESC
             if event.type == pygame.KEYDOWN:
@@ -46,11 +49,17 @@ def main():
 
         player.handle_input()
 
-        # Fondo negro
+        
         game_map.draw(screen)
         treasure.draw(screen)
         player.draw(screen)
+
+        if message != "":
+            text_surface = font.render(message, True, (255, 255, 255))
+            screen.blit(text_surface, (20, 20))
+
         pygame.display.flip()
+
 
     pygame.quit()
     sys.exit()
